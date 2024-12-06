4c922d7ecf2d276558c12c20904c1cdb264cf807
TestingAcount15 <sturningdemetri@gmail.com>
2024-12-05 20:57:30 -0600

    Update allk2eys.lua

--- allk2eys.lua
+++ allk2eys.lua
@@ -1,4 +1,12 @@
-return {
-    { key = "Cdf_LdUVCLNZvrIGcd7fu=T1pSFECZ=iS=S6r88Y13f07aApUI", username = "gotham1.0" },
-    { key = "1drj7cknxqwboaaWzPGWQoa5mCnj-NyANA-OG4fMtrIQRv2-XR", username = "tomboy87" }
+{
+    "1164386504412500009": {
+        "username": "gotham1.0",
+        "key": "Cdf_LdUVCLNZvrIGcd7fu=T1pSFECZ=iS=S6r88Y13f07aApUI",
+        "last_reset": 1733451538.7032313
+    },
+    "1423969364739230009": {
+        "username": "tomboy87",
+        "key": "1drj7cknxqwboaaWzPGWQoa5mCnj-NyANA-OG4fMtrIQRv2-XR",
+        "last_reset": 1733452000.0000000
+    }
 }
