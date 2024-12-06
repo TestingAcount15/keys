7cdf7fcbdd54ec6df88714e8e907b0b7a449f523
TestingAcount15 <sturningdemetri@gmail.com>
2024-12-05 21:21:13 -0600

    Update allkeys.lua

--- allkeys.lua
+++ allkeys.lua
@@ -1,4 +1,1 @@
-return {
-    { key = "Cdf_LdUVCLNZvrIGcd7fu=T1pSFECZ=iS=S6r88Y13f07aApUI", username = "gotham1.0" },
-    { key = "1drj7cknxqwboaaWzPGWQoa5mCnj-NyANA-OG4fMtrIQRv2-XR", username = "tomboy87" }
-}
+
