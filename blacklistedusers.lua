f04e1d1e28ee2c42832d5eb45246b52d9a13c7ab
TestingAcount15 <sturningdemetri@gmail.com>
2024-12-07 16:11:21 -0600

    Update blacklistedusers.lua with the latest changes

--- blacklistedusers.lua
+++ blacklistedusers.lua
@@ -1,3 +1,4 @@
 return {
-    { username = "te#6238", key = "None" }
+    { username = "te#6238", key = "None" },
+    { username = "gotham1.0", key = "2tQ=6xLbYTdeWGwN2py6gefrVFCX9hsXFKjkRVBthl=T_Ko9vu" }
 }
