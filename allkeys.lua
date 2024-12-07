c2fb3ee81d4c602a4bb894b6d988b6d1aeaa10ca
TestingAcount15 <sturningdemetri@gmail.com>
2024-12-06 19:41:44 -0600

    Update allkeys.lua with the latest changes

--- allkeys.lua
+++ allkeys.lua
@@ -1,4 +1,3 @@
 return {
-    { key = "gzRLjEN0bQ9N1vbClYnHDUCcYl4jfxX9Sd_73ZkUKvnTO3Np-r", username = "gotham1.0" },
-    { key = "jQKYDVx_YAALKprnl3YXxrixWSNRmcjMpcu26gCSpksRB2qm8Y", username = "jackatlife101054" }
+    { key = "gzRLjEN0bQ9N1vbClYnHDUCcYl4jfxX9Sd_73ZkUKvnTO3Np-r", username = "gotham1.0" }
 }
