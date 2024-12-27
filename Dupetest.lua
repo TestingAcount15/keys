980ff37ca8a668ebe71ed49b8ce98c77c24ad737
TestingAcount15 <sturningdemetri@gmail.com>
2024-12-27 11:07:19 -0600

    Update Dupetest.lua

--- Dupetest.lua
+++ Dupetest.lua
@@ -1,11 +1,16 @@
-local allKeys = loadstring(game:HttpGet("https://raw.githubusercontent.com/TestingAcount15/keys/refs/heads/main/allkeys.lua"))()
-local blacklistedUsers = loadstring(game:HttpGet("https://raw.githubusercontent.com/TestingAcount15/keys/refs/heads/main/blacklistedusers.lua"))()
+local function fetchLatestData()
+    local allKeys = loadstring(game:HttpGet("https://raw.githubusercontent.com/TestingAcount15/keys/refs/heads/main/allkeys.lua"))()
+    local blacklistedUsers = loadstring(game:HttpGet("https://raw.githubusercontent.com/TestingAcount15/keys/refs/heads/main/blacklistedusers.lua"))()
+    return allKeys, blacklistedUsers
+end
 
 local function checkKey(whitelist_key)
     if not whitelist_key or whitelist_key == "" then
         print("No key entered. Please provide a valid key to continue.")
         return false
     end
+
+    local allKeys, blacklistedUsers = fetchLatestData()
 
     for _, data in pairs(blacklistedUsers) do
         if data.key == whitelist_key then
