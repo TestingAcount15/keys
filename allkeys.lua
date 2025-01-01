6dde608a6e76761288abd1d1caf8ebadbc7a5893
TestingAcount15 <sturningdemetri@gmail.com>
2025-01-01 00:40:43 -0600

    Update allkeys.lua

--- allkeys.lua
+++ allkeys.lua
@@ -1,5 +1,4 @@
 return {
     { key = "2851zUBKS6CWX3CCi=bgJdbc3uHeF26KrWOXlQSYjg1WU9nffn", username = "gotham1.0" },
-    { key = "VaxzKeys123!@", username = "mm632" },
-    { key = "blurthispartplease", username = "IslandDuper Owner" }
+    { key = "VaxzKeys123!@", username = "mm632" }
 }
