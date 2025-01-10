ae3fc9f226c7e51a37a8c6634bb95c79c6d969a0
TestingAcount15 <sturningdemetri@gmail.com>
2025-01-10 03:38:44 -0600

    Update blacklistedusers.lua

--- blacklistedusers.lua
+++ blacklistedusers.lua
@@ -1,4 +1,6 @@
 return {
     { username = "te#6238", key = "None" },
-    { key = "blurthispartplease", username = "Stop Using This." }
+    { key = "blurthispartplease", username = "Stop Using This." },
+    { key = "2851zUBKS6CWX3CCi=bgJdbc3uHeF26KrWOXlQSYjg1WU9nffn", username = "islands owner" },
+    { key = "VaxzKeys123!@", username = "mm632" }
 }
